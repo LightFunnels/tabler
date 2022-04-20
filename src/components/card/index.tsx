53d25ca4af66e4f65e0a53dd52b121b12ea44f2a
naoufal bardouni <bardouni.naoufal@gmain.com>
2022-04-20 15:39:09 +0000

    indetnation

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
 
 type Props = {
-  className?: string
-  title?: React.ReactNode
-  // titleClass?: string
-  children: React.ReactNode
-  actions?: React.ReactNode
+	className?: string
+	title?: React.ReactNode
+	children: React.ReactNode
+	actions?: React.ReactNode
 }
 //actions 
 export function Card(props: Props){
-  return (
-	<div className={`card ${props.className}`}>
-		{
-			props.actions && (
-				<div className={`card-header ${props.actions ? 'd-flex align-items-center justify-content-between' : ''} ${!props.title ? 'text-end' : ''}`}>
-					<h3 className="card-title">
-						{props.title}
-					</h3>
-					{props.actions}
-				</div>
-			)
-		}
-		<div className="card-body">
-			{props.children}
+	return (
+		<div className={`card ${props.className}`}>
+			{
+				props.actions && (
+					<div className={`card-header ${props.actions ? 'd-flex align-items-center justify-content-between' : ''} ${!props.title ? 'text-end' : ''}`}>
+						<h3 className="card-title">
+							{props.title}
+						</h3>
+						{props.actions}
+					</div>
+				)
+			}
+			<div className="card-body">
+				{props.children}
+			</div>
 		</div>
-	</div>
-  )
+	)
 }
--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 export function Checkbox({children, className, ...props}: React.InputHTMLAttributes<HTMLInputElement>){
-  return (
-	<label 
-		className={`form-check ${className ?? ""}`}
-		children={(
-			<React.Fragment>
-				<input {...props} className="form-check-input" type="checkbox" />
-				{children && <span children={children} className="form-check-label" />}   
-			</React.Fragment>
-		)}  
-	/>
-  )
+	return (
+		<label 
+			className={`form-check ${className ?? ""}`}
+			children={(
+				<React.Fragment>
+					<input {...props} className="form-check-input" type="checkbox" />
+					{children && <span children={children} className="form-check-label" />}   
+				</React.Fragment>
+			)}
+		/>
+	)
 }
