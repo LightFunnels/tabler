6c8596d78973c8dd8dc22dd1eece434934a58136
omarhmitoch <60047489+omarhmitoch@users.noreply.github.com>
2022-07-07 10:23:56 +0100

    component style change

--- src/components/page-header/index.tsx
+++ src/components/page-header/index.tsx
@@ -6,40 +6,43 @@ type Props = {
 	subHeader?: React.ReactNode 
 	breadcrumbs?: React.ReactNode
 }
+import styles from './page-header.scss';
 
 export function PageHeader(props : Props) {
   return (
-    <div className="container-xl">
-      <div className="page-header mt-5 mb-4">
-        <div className="row g-2 align-items-center">
-					{
-						props.breadcrumbs && (
-							<ol className="breadcrumb">
-								{props.breadcrumbs}
-							</ol>
-						)
-					}
-					<div className="col">
+		<div className={styles.pageHeader}>				
+			<div className="container-xl">
+				<div className="page-header mt-5 mb-4">
+					<div className="row g-2 align-items-center">
 						{
-							props.subHeader && (
-								<div className="page-pretitle">{props.subHeader}</div>
+							props.breadcrumbs && (
+								<ol className="breadcrumb">
+									{props.breadcrumbs}
+								</ol>
 							)
 						}
-						<h1 className="page-title">{props.title}</h1>
-					</div>
-					{
-						props.actions && (
-							<div className="col-12 col-md-auto ms-auto d-print-none">
-								<div className="btn-list">
-									<span className="d-flex align-items-center">
-										{props.actions}
-									</span>
+						<div className="col">
+							{
+								props.subHeader && (
+									<div className="page-pretitle">{props.subHeader}</div>
+								)
+							}
+							<h1 className="page-title">{props.title}</h1>
+						</div>
+						{
+							props.actions && (
+								<div className="col-12 col-md-auto ms-auto d-print-none">
+									<div className="btn-list">
+										<span className="d-flex align-items-center">
+											{props.actions}
+										</span>
+									</div>
 								</div>
-							</div>
-						)
-					}
-        </div>
-      </div>
-    </div>
+							)
+						}
+					</div>
+				</div>
+			</div>
+		</div>
   );
 }
